162df85bfb24474da819bba74fc6845003a600f4
Artemiy Skryabin <artemiy.skryabin@pimpay.ru>
2022-12-12 21:44:53 +0300

    f

--- index.js
+++ index.js
@@ -4,14 +4,19 @@ import {sleep} from './helpers.js'
 
 let arr = [];
 let badarr = [];
-const JOB = 'Product Manager';
+
 const LOCATION = {
     'Portugal': '&loc=Portugal&latlng=40.19788004933705%2C-8.972960422448347',
     'Germany': '&loc=Germany&latlng=50.686802796928%2C9.546094288265737',
     'Remote': '&remote=true'
 }
 
+//edit here
+const JOB = 'Product Manager';
+const PAGES = 20
+const location = LOCATION.Portugal
 const BANWORDS = ['junior', 'principal', 'head'];
+//
 
 const webdriver = new Builder()
     .withCapabilities(Capabilities.firefox())
@@ -45,9 +50,7 @@ async function pageRun(page = 1, loc) {
 
 (async function () {
     //количество страниц
-    const pages = 20
-    const location = LOCATION.Portugal
-    for (let i = 1; i <= pages; i++) {
+    for (let i = 1; i <= PAGES; i++) {
         try {
             await pageRun(i, location);
         } catch (e) {
@@ -57,7 +60,7 @@ async function pageRun(page = 1, loc) {
     console.log('Исключили вакансии с таким названием:')
     console.log(badarr)
     console.log(arr)
-    console.log(`Собрано ${arr.length} ссылок по запросу ${JOB} на ${pages} страницах`)
+    console.log(`Собрано ${arr.length} ссылок по запросу ${JOB} на ${PAGES} страницах`)
     console.log(`Исключили по названию ${badarr.length}`)
     await webdriver.quit()
 })();
