62ddb860ebd3aa3f704999c8707a8c7d2e136803
Artemiy Skryabin <artemiy.skryabin@pimpay.ru>
2022-12-12 21:33:14 +0300

    f

--- index.js
+++ index.js
@@ -3,29 +3,62 @@ import {Builder, Capabilities, By} from 'selenium-webdriver';
 import {sleep} from './helpers.js'
 
 let arr = [];
-const JOB = 'qa';
+let badarr = [];
+const JOB = 'Product Manager';
+const LOCATION = {
+    'Portugal': '&loc=Portugal&latlng=40.19788004933705%2C-8.972960422448347',
+    'Germany': '&loc=Germany&latlng=50.686802796928%2C9.546094288265737',
+    'Remote': '&remote=true'
+}
+
+const BANWORDS = ['junior', 'principal', 'head']
 
 const webdriver = new Builder()
     .withCapabilities(Capabilities.firefox())
     .build()
 
-async function pageRun(page = 1) {
-    await webdriver.get(`https://startup.jobs/?q=${JOB}&page=${page}`)
+async function pageRun(page = 1, loc) {
+    await webdriver.get(`https://startup.jobs/?q=${JOB}&page=${page}${loc}`)
     await sleep(2000);
 
-    for (let i = 1; i < 25; i++) {
-        await webdriver.findElement(By.xpath(`/html/body/section[2]/div/div/div[2]/div[${i}]/div[1]/div/a[1]`)).getAttribute('href').then(
-            (link) => arr.push(link)
-        )
+    for (let i = 1; i < 26; i++) {
+        let xpath = `/html/body/section[2]/div/div/div[2]/div[${i}]/div[1]/div/`
+        let element = await webdriver.findElement(By.xpath(`${xpath}a[1]`))
+
+        let append = true;
+        await element.getText().then(
+            text => BANWORDS.some(el => {
+                if(append){
+                    append = !text.toLowerCase().includes(el);
+                } else {
+                    badarr.push(text) //for test
+                }
+            }))
+
+        await element.getAttribute('href').then(link =>{
+            if(append){
+                arr.push(link)
+            }
+        })
     }
 }
 
 (async function () {
-    for (let i = 1; i <= 4; i++) {
-        await pageRun(i);
+    //количество страниц
+    const pages = 20
+    const location = LOCATION.Portugal
+    for (let i = 1; i <= pages; i++) {
+        try {
+            await pageRun(i, location);
+        } catch (e) {
+            throw new Error('Указано много страниц')
+        }
     }
+    console.log('Исключили вакансии с таким названием:')
+    console.log(badarr)
     console.log(arr)
-    console.log(`Собрано ${arr.length} ссылок по запросу ${JOB}`)
+    console.log(`Собрано ${arr.length} ссылок по запросу ${JOB} на ${pages} страницах`)
+    console.log(`Исключили по названию ${badarr.length}`)
     await webdriver.quit()
 })();
 
